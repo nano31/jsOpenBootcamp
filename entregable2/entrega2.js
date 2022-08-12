cab291409b57fa02246707cf5794a80747c93ce6
Franco Garcia Carril <78042300+nano31@users.noreply.github.com>
2022-08-12 16:41:46 -0300

    hago arreglos y entregable 2

---
+++ entregable2/entrega2.js
@@ -0,0 +1,11 @@
+const datos = ['Franco',
+21,
+false,
+new Date(2001,5,31),
+{
+  titulo: 'Aristoteles y Dante descubrem los secretos del universo',
+  autor: 'Benjamin Alire Saenz',
+  fecha: new Date(2015,8,1),
+  url: 'https://www.planetadelibros.com/libro-aristoteles-y-dante-descubren-los-secretos-del-universo/293694'
+},
+];
